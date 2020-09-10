4348a22748363dfb1358cad2a07e555e7f86c416
Wilco van Dijk <wilcodijkvan@gmail.com>
2020-09-11 00:12:08 +0200

    updated name

---
+++ wielerkom/Advertisements/Entities/Advertisement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace fietskom.Advertisements.Entities
+{
+    public class Advertisement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
